302106bd5f27cf22dddbf46c5d1735629544df5a
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-10 16:38:58 +0000

    Sicheres Formular erstellen / Create secure form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.app')
 
-@section('title', 'Add Task')
+@section('title', 'Aufgabe hinzufügen')
 
 @section('content')
 
-<form method="POST" action="{{routr('tasks.store')}}">
+<form method="POST" action="{{route('tasks.store')}}">
 @csrf
+    <div>
+        <label for="title">Titel</label>
+        <input type="text" name="title" id="title">
+    </div>
+    <div>
+    <label for="beschreibung">Beschreibung</label>
+    <textarea name="beschreibung" id="beschreibung" rows="5"></textarea>
+    </div>
+    <div>
+    <label for="lang_beschreibung">lang Beschreibung</label>
+    <textarea name="lang_beschreibung" id="lang_beschreibung" rows="5"></textarea>
+    </div>
+    <div>
+        <button type="submit">Aufgabe hinzufügen</button>
+    </div>
 </form>
 
 @endsection
