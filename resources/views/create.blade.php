21e94856510609bbd70bedb0ae3a67a645eae4c6
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-11 19:13:57 +0000

    Ich erstele @section('style') für Fahlernachrichten  / Create style to error message

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,7 +4,14 @@
 
 @section('content')
 
-
+@section('styles')
+    <style>
+        .fahler-nachricht{
+            color: red;
+            font-size: 0, 8rem;
+        }
+    </style>
+@endsection
 
 <form method="POST" action="{{route('tasks.store')}}">
 @csrf
@@ -12,21 +19,21 @@
         <label for="title">Titel</label>
         <input type="text" name="title" id="title">
         @error('title')
-            <p>{{"Das Feld Titel ist erforderlich."}}</p>
+            <p class="fahler-nachricht">{{"Das Feld Titel ist erforderlich."}}</p>
         @enderror
     </div>
     <div>
         <label for="beschreibung">Beschreibung</label>
         <textarea name="beschreibung" id="beschreibung" rows="5"></textarea>
         @error('beschreibung')
-            <p>{{"Das Feld Beschreibung ist erforderlich."}}</p>
+            <p class="fahler-nachricht">{{"Das Feld Beschreibung ist erforderlich."}}</p>
         @enderror
     </div>
     <div>
         <label for="lang_beschreibung">lang Beschreibung</label>
         <textarea name="lang_beschreibung" id="lang_beschreibung" rows="10"></textarea>
         @error('lang_beschreibung')
-            <p>{{"Das Feld lang Beschreibung ist erforderlich."}}</p>
+            <p class="fahler-nachricht">{{"Das Feld lang Beschreibung ist erforderlich."}}</p>
         @enderror
     </div>
     <div>
