0f256b9a577ba4125f19d400b05c1540fe13c363
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-04 09:30:22 +0000

    Rück-Link zur Aufgabenliste * Tailwind CSS hinzugefügt / Added Tailwind CSS * Added back link to task list

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,41 +3,43 @@
 @section('title', $task->title)
 
 @section('content')
+
+<div class="mb-4">
+    <a href="{{ route('tasks.index') }}"
+            class="link">← Zurück zur Aufgabenliste</a>
+</div>
 {{-- <h1>{{$task->title}}</h1> --}}
-<p>{{$task->Beschreibung}}</p>
+<p class="mb-4 text-slate-700">{{$task->Beschreibung}}</p>
 
 @if ($task->lang_Beschreibung)
-    <p>{{$task->lang_Beschreibung}}</p>
+    <p class="mb-4 text-slate-700">{{$task->lang_Beschreibung}}</p>
 @endif
 
-<p>{{$task->created_at}}</p>
-<p>{{$task->updated_at}}</p>
+<p class="mb-4 text-sm text-slate-500">Erstellen : {{$task->created_at->diffForHumans()}} • Bearbeiten : {{$task->updated_at->diffForHumans()}}</p>
 
-<p>
+<p class="mb-4">
     @if ($task->abgeschlossen)
-        Abgeschlossen
+        <span class="font-medium text-green-500">Abgeschlossen</span>
     @else
-        Unvollständig
+        <span class="font-medium text-red-500">Unvollständig</span>
     @endif
 </p>
-<div>
-    <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">Bearbeiten</a>
-</div>
+<div class="flex gab-3">
+    <a href="{{ route('tasks.edit', ['task' => $task->id]) }}"
+        class="btn">Bearbeiten</a>
 
-<div>
     <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
         @csrf
         @method('PUT')
-        <button type="submit">
+        <button type="submit" class="btn">
             {{ $task->abgeschlossen ? 'Als unvollständig markieren' : 'Als abgeschlossen markieren' }}
         </button>
     </form>
-</div>
-<div>
+
     <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
         @csrf
         @method('DELETE')
-        <button type="submit"> Löschen </button>
+        <button type="submit" class="btn"> Löschen </button>
     </form>
 </div>
 @endsection
