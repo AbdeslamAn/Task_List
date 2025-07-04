0d26df367c5dcc6f2932910ff65f4521294e4d3d
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-04 07:16:43 +0000

    Fomrular erstellt, um das Umschalten (toggle) umzusetzen / Form created to implement the toggle

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,8 +13,25 @@
 <p>{{$task->created_at}}</p>
 <p>{{$task->updated_at}}</p>
 
+<p>
+    @if ($task->abgeschlossen)
+        Abgeschlossen
+    @else
+        Unvollständig
+    @endif
+</p>
 <div>
     <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">Bearbeiten</a>
+</div>
+
+<div>
+    <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
+        @csrf
+        @method('PUT')
+        <button type="submit">
+            {{ $task->abgeschlossen ? 'Als unvollständig markieren' : 'Als abgeschlossen markieren' }}
+        </button>
+    </form>
 </div>
 <div>
     <form action="{{ route('tasks.destroy', ['task' => $task->id]) }}" method="POST">
