c9c421b3e01c5fae6a19c8a62a433e27150976f5
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-06 07:49:41 +0000

    Flash-Nachricht gestaltet und Schließen-Button mit Alpine.js hinzugefügt / Added flash message styling and close button using Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 12 Task List App</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
@@ -13,7 +14,7 @@
             @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-green-500 bg-green-300
         }
         .btn2{
-            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-red-500 bg-red-300 
+            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-red-500 bg-red-300
         }
         .link{
             @apply font-medium text-gray-700 underline decoration-pink-500
@@ -33,13 +34,27 @@
 </head>
 <body class="container mx-auto mt-10 max-w-lg" >
 
-
-    @if (session()->has('succes'))
-        <div>{{session('succes')}}</div>
-    @endif
     <h1 class="mb-4 text-Zxl">@yield('title')</h1>
 
-    <div>
+    <div x-data="{flash: true}">
+          @if (session()->has('succes'))
+
+        <div x-show="flash"
+            class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                    role="alert">
+            <strong class="font-bold">Erfolg!</strong>
+            <<div>{{session('succes')}}</div>
+
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" @click="flash = false"
+                    stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </span>
+        </div>
+
+        @endif
         @yield('content')
     </div>
 </body>
